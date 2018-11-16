72cc877bec9800fc2363ec66a57af3871ce76264
cyberspeck <cyberspeck@users.noreply.github.com>
2018-11-16 10:34:02 +0100

    include hpd

--- ue2_20181116/ue2_5.py
+++ ue2_20181116/ue2_5.py
@@ -2,9 +2,13 @@
 
 ##!/usr/bin/env python3
 
+""" Skeleton file, das eure Lernkurve beschleunigen soll,
+    fuer Uebung 2.5.
+"""
 import sys
 import numpy, scipy.misc, math, scipy.integrate, scipy.stats, scipy.optimize
 from matplotlib import pyplot as plt
+#import pymc3
 
 n=60
 s=124
@@ -21,6 +25,21 @@ def find_max(func, a, b, precision = 6):
         a += 1/(10**precision)
     return round(pos_max, precision)
 
+def hpd_calc_symmetrical(func, alpha, precision = 0.001, start = 0):
+    integral = 0
+    if alpha >= 1 or alpha <= 0:
+        raise ValueError('alpha must be between 0 and 1. It is {}'.format(alpha))
+    limit = (1-alpha)+ (alpha/2)
+    hpd = [-1,-1]
+    x = start
+    while integral < limit:
+        integral += func(x)*precision
+        if hpd[0] == -1:
+            if integral >= alpha/2:
+                hpd[0] = round(x,3)
+        x += precision
+    hpd[1] = round(x, 3)
+    return hpd
 
 if __name__ == '__main__':
     print("---------------------------------------------------------------\n")
@@ -34,14 +53,15 @@ if __name__ == '__main__':
     denominator = scipy.integrate.quad(prior_x_likelyhood, 0, 305)
     #denominator = scipy.integrate.quad(denominator, 0, numpy.inf)  #führt zu overflow innerhalb der funktion
     posterior = lambda l: prior_x_likelyhood(l)/denominator[0]
+    #normieren:
+    norm_factor = scipy.integrate.quad(posterior,1 ,300)[0]
+    posterior = lambda l: prior_x_likelyhood(l)/(denominator[0]*norm_factor)
     bayes = find_max(posterior, 1.8, 2.2)
-    print(bayes)
-
-
-
-
-    dataPoints = numpy.linspace(0, 4, 1000)
+    print("Bayesian Expected Value: {}".format(bayes))
+    #print(scipy.integrate.quad(posterior, 1, 300))
+    print(hpd_calc_symmetrical(posterior, 0.05))
     ### plot
+    dataPoints = numpy.linspace(0, 4, 1000)
     plt.plot(dataPoints, prior(dataPoints), label="prior")
     #plt.plot(dataPoints, prior(dataPoints), label="likelihood")
     plt.plot(dataPoints, posterior(dataPoints), label='postrior')
