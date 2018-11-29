50a063a2a6ac4a04b637588afd5b06ab4b86fbce
cyberspeck <cyberspeck@users.noreply.github.com>
2018-11-29 17:49:20 +0100

    small changes

--- ue4_20181130/ue4_5_core.py
+++ ue4_20181130/ue4_5_core.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 from matplotlib import style
-style.use('fivethirtyeight')
 
 np.random.seed(0)
 
+iterations = 10
 n = 250
 mu = [0, 0]
 sigma = [1, np.sqrt(10)]
@@ -21,33 +21,33 @@ def gaussian_mix(sample_size):
 X_tot = gaussian_mix(n)
 
 class GM1D:
-    def __init__(self,X,iterations):
+    def __init__(self,sample,iterations):
         self.iterations = iterations
-        self.X = X
+        self.sample = sample
         self.mu = None
         self.pi = None
         self.var = None
-  
+
     def run(self):
-        
+
         """
         Start with arbitrary mu, pi and var
         """
-        self.mu = [-8,5]
+        self.mu = [0,0]
         self.pi = [1/2,1/2]
-        self.var = [5,1]
-        
-        
+        self.var = [1,5]
+
+
         """
         E-Step
         """
-        
+
         for iter in range(self.iterations):
             """Create and calculate probability for each datapoint x_i to belong to gaussian g"""
-            r = np.zeros((len(X_tot),2))  
+            r = np.zeros((len(X_tot),2))
             for column,g,weight in zip(range(2), [norm(loc=self.mu[0],scale=self.var[0]),
                                                   norm(loc=self.mu[1],scale=self.var[1])], self.pi):
-                # Write the probability that x belongs to gaussian c in column c. 
+                # Write the probability that x belongs to gaussian c in column c.
                 r[:,column] = weight*g.pdf(X_tot)
 
             """
@@ -62,32 +62,40 @@ class GM1D:
                 fig = plt.figure(figsize=(10,10))
                 ax0 = fig.add_subplot(111)
                 for i in range(len(r)):
-                    ax0.scatter(self.X[i],0,s=100) 
-                    #ax0.scatter(self.X[i],0,c=np.array([r[i][0],r[i][1],r[i][2]]),s=100) 
+                    ax0.scatter(self.sample[i],0,s=100)
+                    #ax0.scatter(self.sample[i],0,c=np.array([r[i][0],r[i][1],r[i][2]]),s=100)
                 """Plot the gaussians"""
                 for g,c in zip([norm(loc=self.mu[0],scale=self.var[0]).pdf(np.linspace(-20,20,num=n)),
                                 norm(loc=self.mu[1],scale=self.var[1]).pdf(np.linspace(-20,20,num=n))],['r','b']):
                     ax0.plot(np.linspace(-20,20,num=n),g,c=c)
                 plt.show()
-            
-            
+
+
             """M-Step"""
-    
-            """calculate m_c"""
+
+            # For each cluster c, calculate the m_c and add it to the list m_c
             m_c = []
             for c in range(len(r[0])):
                 m = np.sum(r[:,c])
-                m_c.append(m) # For each cluster c, calculate the m_c and add it to the list m_c
-            """calculate pi_c"""
+                m_c.append(m)
+
+            # For each cluster c, calculate the fraction of points pi_c which belongs to cluster c
             for k in range(len(m_c)):
-                self.pi[k] = (m_c[k]/np.sum(m_c)) # For each cluster c, calculate the fraction of points pi_c which belongs to cluster c
-            """calculate mu_c"""
-            self.mu = np.sum(self.X.reshape(len(self.X),1)*r,axis=0)/m_c
-            """calculate var_c"""
-            var_c = []
+                self.pi[k] = (m_c[k]/np.sum(m_c))
+
+            # calculate mu_c
+            self.mu = np.sum(self.sample.reshape(len(self.sample),1)*r,axis=0)/m_c
+
+            # calculate var_c
+            var_c = np.empty(len(r[0]))
             for c in range(len(r[0])):
-                var_c.append( (1/m_c[c]) * np.dot(((np.array(r[:,c]).reshape(n,1)) * (self.X.reshape(len(self.X),1)-self.mu[c])).T,
-                                                  (self.X.reshape(len(self.X),1)-self.mu[c])) )
-    
-GM1D = GM1D(X_tot,20)
+                var_c[c] = (1/m_c[c]) * np.dot(((np.array(r[:,c]).reshape(n,1)) * (self.sample.reshape(len(self.sample),1)-self.mu[c])).T,
+                                                  (self.sample.reshape(len(self.sample),1)-self.mu[c]))
+            self.var = var_c
+
+GM1D = GM1D(X_tot,iterations)
 GM1D.run()
+
+print("mu= {}".format(GM1D.mu))
+print("var= {}".format(GM1D.var))
+print("pi= {}".format(GM1D.pi))
